bce53a63b15e49d7b0ad5a904f7ec5c15d740678
vedika123-A <97055547+vedika123-A@users.noreply.github.com>
2022-01-04 23:04:13 +0530

    Create 2.cpp

---
+++ Goldman Sachs/2.cpp
@@ -0,0 +1,13 @@
+class Solution {
+ public:
+   int doOverlap(int L1[], int R1[], int L2[], int R2[]) {
+       // code here
+       //If one rectangle is on left side of the left edge of the other rectangle
+       if(L1[0]>R2[0] || L2[0]>R1[0])
+           return false;
+       //If one rectangle is above the other
+       if(L1[1]<R2[1] || L2[1]<R1[1])
+           return false;
+       return true;
+   }
+};
