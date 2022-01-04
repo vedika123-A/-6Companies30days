c0ef3001b7634a5a94ac140e5602acc2324bd49c
vedika123-A <97055547+vedika123-A@users.noreply.github.com>
2022-01-04 23:20:56 +0530

    Create 13.cpp

---
+++ Goldman Sachs/13.cpp
@@ -0,0 +1,36 @@
+class Solution{
+public:
+    string decodedString(string s){
+       stack<char>st;
+       string ans;
+       for(int i=0;i<s.size();i++){
+           if(s[i]!=']') st.push(s[i]);
+           else{
+               string temp;
+               while(!st.empty() && st.top()!='['){
+                  temp = st.top() + temp;
+                st.pop();
+               }
+               st.pop();
+            string num;
+            while (!st.empty() && isdigit(st.top())) {
+                num = st.top() + num;
+                st.pop();
+            }
+            int number = stoi(num);
+            string repeat;
+            for (int j = 0; j < number; j++)
+                repeat += temp;
+            for (char c : repeat)
+                st.push(c);
+           }
+           
+       }
+       string res;
+    while (!st.empty()) {
+        res = st.top() + res;
+        st.pop();
+    }
+    return res;
+      }
+  };
